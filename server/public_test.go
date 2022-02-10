853bbd93aee1224159746cf4219aa806bcc33a6e
MeHDI Rh <38499444+mehdiirh@users.noreply.github.com>
2022-02-10 20:06:57 +0330

    Update public_test.go

--- server/public_test.go
+++ server/public_test.go
@@ -979,7 +979,7 @@ func socketioTestsBitcoinType(t *testing.T, ts *httptest.Server) {
 		{
 			name: "socketio getInfo",
 			req:  socketioReq{"getInfo", []interface{}{}},
-			want: `{"result":{"blocks":225494,"testnet":true,"network":"fakecoin","subversion":"/Fakecoin:0.0.1/","coin_name":"Fakecoin","about":"Blockbook - blockchain indexer for Trezor wallet https://trezor.io/. Do not use for any other purpose."}}`,
+			want: `{"result":{"blocks":225494,"testnet":true,"network":"fakecoin","subversion":"/Fakecoin:0.0.1/","coin_name":"Fakecoin","about":"Blockchain indexer for CoinoPal https://coinopal.com. Do not use for any other purpose."}}`,
 		},
 		{
 			name: "socketio estimateFee",
